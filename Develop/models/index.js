1816b501290ac4bf9a69ec437cea9ab3b5fb6bd3
Katy Thompson <katimation@gmail.com>
2024-01-04 14:46:04 -0600

    added README

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,28 +6,23 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'id',
+  foreignKey: 'category_id',
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'id',
+  foreignKey: 'category_id',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'products_with_tags'
+  through: ProductTag,
+  foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'tags_for_products'
+  through: ProductTag,
+  foreignKey: 'tag_id'
 });
+
 module.exports = {
   Product,
   Category,
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # e-commerce-back-end
+
+## Description
+This project is the built back end for an e-commerce site.
+
+## Installation
+Use npm install then npm start to run the program on your local host.
+
+## Usage
+The ideal user of this program would be the owners of the e-commerce site. 
+
+## License
+![License: MIT License](https://img.shields.io/badge/License-MIT-red)
+
+## Deployment
+(![Screenshot of the program tested in Insomnia](Develop/images/e-commerce.png))
+
+## Contact Information
+GitHub Username: katycaroline
+Email: katimation@gmail.com
